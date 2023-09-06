31a073a0f163fb3ee3d349b2c89bf4bd54842b57
alaaelnaggar72 <elnaggar@gmail.com>
2023-09-06 21:53:53 +0300

    b8-Use-validatore.ts-To-matchpassword-With-FormGroup

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CustvalidatorService } from '../services/-custvalidator.service';
 
 
 import {
 Validators,
   FormBuilder,
+  FormGroup,
+  FormControl,
 } from '@angular/forms';
+import { matchpassword } from 'src/matchPassword.validators';
 
 @Component({
   selector: 'app-sign-up',
@@ -18,35 +20,19 @@ export class SignUpComponent {
   constructor(
     public fb: FormBuilder,
     public _router: Router,
-    public _CustvalidatorService:CustvalidatorService
   ) {}
 
-  userForm = this.fb.group(
-    {
-      name: ['', [Validators.required, Validators.minLength(5)]],
-      email: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern('[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[a-zA-Z]{2,4}$'),
-        ],
-      ],
-      userName: [
-        '',
-        [Validators.required, Validators.pattern('^[a-zA-Z0-9]*$')],
-      ],
 
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-    }
-    ,
-    {
-      validator: this._CustvalidatorService.passwordMatchValidator(
-        'password',
-        'confirmPassword'
-      ),
-    }
-  );
+
+
+userForm: FormGroup = new FormGroup({
+    name: new FormControl('',[Validators.required,Validators.minLength(5)]),
+    email: new FormControl('',[Validators.required,Validators.pattern('[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-zA-Z]{2,4}$')]),
+    userName: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z0-9]*$')]),
+    password: new FormControl(null,[Validators.required,Validators.minLength(3)]),
+    confirmPassword: new FormControl(null,[Validators.required]),
+  },{validators:matchpassword});
+
 
   submitUserForm() {
     console.log(this.userForm);
